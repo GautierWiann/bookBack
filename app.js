91e45e27264c3b0cdf96474e19fcdb7f73d39bb0
Wianni Gautier <wianni.gautier@gmail.com>
2022-09-01 12:47:01 +0200

    rien

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var session = require("express-session");
+
 var app = express();
+app.use(
+  session({
+   secret: 'a4f8071f-c873-4447-8ee2',
+   resave: false,
+   saveUninitialized: false,
+  })
+  );
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +31,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-var session = require("express-session");
 
 
 // catch 404 and forward to error handler
